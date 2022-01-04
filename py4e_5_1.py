d3c401b578e316906a5d8d6bf1257bd1c8f05e06
Matt McGee <mlmcgeenc@gmail.com>
2022-01-04 11:24:07 -0500

    finish exercise 5,1

--- py4e_5_1.py
+++ py4e_5_1.py
@@ -1,1 +1,19 @@
 print("PY4E exercise 5.1")
+
+num = 0
+total = 0.0
+while True :
+  sval = input('Enter a number: ')
+  if sval == 'done' :
+    break
+  try:
+    fval = float(sval)
+  except:
+    print('Invalid input.')
+    continue
+  print(fval)
+  num = num + 1
+  total = total + fval
+
+print('ALL DONE')
+print(total, num, total/num)
